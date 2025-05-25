e4a44abd1bb4a0cc58c3617d6d4fc540be20ca90
CJZurc <cruz.cristianjames.ambrocio@gmail.com>
2025-05-25 23:09:00 +0800

    added pos table

---
+++ migrations/002_create_pos_table.py
@@ -0,0 +1,29 @@
+import sqlite3
+import os
+
+db_name = "mare_kwenta.db"
+
+# Check if the database file exists
+if not os.path.exists(db_name):
+    print("Database doesn't exist. Creating a new one.")
+else:
+    print("Database exists. Proceeding with migration.")
+
+try:
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS pos (
+            pos_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            product_name VARCHAR(255) NOT NULL,
+            total_amount INTEGER NOT NULL
+        )
+    """)
+
+    conn.commit()
+except sqlite3.Error as e:
+    print("An error occurred while connecting to SQLite:", e)
+finally:
+    if conn:
+        conn.close()
